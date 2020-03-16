e0013b3ff0fd285a2bf1f774339bed97d72a322e
Casey Zacney <czacney@gmail.com>
2020-03-15 22:53:26 -0400

    hid pw using dotenv

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+require('dotenv').config()
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: process.env.password,
     database: "burgers_db"
 });
 
